000990f2b6300bbd5f6ef529bf8dc5a386d29271
zengjie <zjengjie@gmail.com>
2018-04-02 14:47:44 +0800

    init

---
+++ src/main/java/com/example/gateway/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.session.data.redis.RedisFlushMode;
+import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
+        // @formatter:off
+        return http.httpBasic().and()
+                .authorizeExchange()
+                    .pathMatchers("/login/**").permitAll()
+                .anyExchange().authenticated()
+                .and().formLogin()
+                .and().logout().and()
+                .build();
+        // @formatter:on
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService reactiveUserDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+}
